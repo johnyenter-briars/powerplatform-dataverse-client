445b363b02d174d1c076b83496f94ab57367e693
John Yenter-Briars <johnyenterbriars@protonmail.com>
2026-03-11 06:12:10 -0500

    more params

--- src/dataverse/requestparameters.rs
+++ src/dataverse/requestparameters.rs
@@ -2,12 +2,20 @@ use reqwest::RequestBuilder;
 
 #[derive(Debug, Clone, Default)]
 pub struct RequestParameters {
+    pub bypass_business_logic_execution_custom_sync: bool,
+    pub bypass_business_logic_execution_custom_async: bool,
     pub bypass_custom_plugin_execution: bool,
     pub suppress_callback_registration_expander_job: bool,
+    // TODO:
+    // pub bypass_business_logic_execution_step_ids: Option<Vec<String>>,
 }
 
 impl RequestParameters {
     pub fn apply(&self, mut request: RequestBuilder) -> RequestBuilder {
+        if let Some(value) = self.bypass_business_logic_execution_value() {
+            request = request.header("MSCRM.BypassBusinessLogicExecution", value);
+        }
+
         if self.bypass_custom_plugin_execution {
             request = request.header("MSCRM.BypassCustomPluginExecution", "true");
         }
@@ -16,6 +24,26 @@ impl RequestParameters {
             request = request.header("MSCRM.SuppressCallbackRegistrationExpanderJob", "true");
         }
 
+        // TODO:
+        // if let Some(step_ids) = &self.bypass_business_logic_execution_step_ids {
+        //     request = request.header(
+        //         "MSCRM.BypassBusinessLogicExecutionStepIds",
+        //         step_ids.join(","),
+        //     );
+        // }
+
         request
+    }
+
+    fn bypass_business_logic_execution_value(&self) -> Option<&'static str> {
+        match (
+            self.bypass_business_logic_execution_custom_sync,
+            self.bypass_business_logic_execution_custom_async,
+        ) {
+            (true, true) => Some("CustomSync,CustomAsync"),
+            (true, false) => Some("CustomSync"),
+            (false, true) => Some("CustomAsync"),
+            (false, false) => None,
+        }
     }
 }
